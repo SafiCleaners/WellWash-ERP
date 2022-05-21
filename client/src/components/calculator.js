3dc11918dd794753fa28a192a72672b6b9c636ba
BransonGitomeh <sirbranson67@gmail.com>
2022-05-21 14:02:27 +0300

    chore: more cleanup to the order form. more labels

--- client/src/components/calculator.js
+++ client/src/components/calculator.js
@@ -554,11 +554,11 @@ var calculator = () => {
                                         m("div", { "class": "col-lg-12" },
                                             [
                                                 m("label",
-                                                    "Appartment Name:"
+                                                    "Appartment's Name:"
                                                 ),
                                                 m("div", { "class": "input-group" },
                                                     [
-                                                        m("input", { "class": "form-control", "type": "text", "placeholder": "Title to your paper" }),
+                                                        m("input", { "class": "form-control", "type": "text", "placeholder": "What is your apprtment commonly called?" }),
                                                         m("div", { "class": "input-group-append" },
                                                             m("span", { "class": "input-group-text" },
                                                                 m("i", { "class": "la la-align-center" })
@@ -572,10 +572,30 @@ var calculator = () => {
                                             ]
                                         ),
                                         m("div", { "class": "col-lg-12" },
+                                            [
+                                                m("label",
+                                                    "House Number:"
+                                                ),
+                                                m("div", { "class": "input-group" },
+                                                    [
+                                                        m("input", { "class": "form-control", "type": "text", "placeholder": "Whats the house number?" }),
+                                                        m("div", { "class": "input-group-append" },
+                                                            m("span", { "class": "input-group-text" },
+                                                                m("i", { "class": "la la-align-center" })
+                                                            )
+                                                        )
+                                                    ]
+                                                ),
+                                                m("span", { "class": "form-text text-muted" },
+                                                    "The name of the house in the appartment"
+                                                )
+                                            ]
+                                        ),
+                                        m("div", { "class": "col-lg-12" },
                                             m("div", { "class": "form-group mb-1" },
                                                 [
                                                     m("label", { "for": "exampleTextarea" },
-                                                        "More Detailss"
+                                                        "Any more details you would like us to know about the pickup and dropoff?"
                                                     ),
                                                     m("textarea", { "class": "form-control", "id": "exampleTextarea", "rows": "12", "spellcheck": "true" })
                                                 ]
@@ -583,34 +603,34 @@ var calculator = () => {
                                         ),
                                         m("br"),
                                         m("br"),
-                                        m("div", { "class": "col-lg-12" },
-                                            m("div", { "class": "alert alert-custom alert-default", "role": "alert" },
-                                                [
-                                                    m("div", { "class": "alert-icon" },
-                                                        m("span", { "class": "svg-icon svg-icon-primary svg-icon-xl" },
-                                                            m("svg", { "xmlns": "http://www.w3.org/2000/svg", "xmlns:xlink": "http://www.w3.org/1999/xlink", "width": "24px", "height": "24px", "viewBox": "0 0 24 24", "version": "1.1" },
-                                                                m("g", { "stroke": "none", "stroke-width": "1", "fill": "none", "fill-rule": "evenodd" },
-                                                                    [
-                                                                        m("rect", { "x": "0", "y": "0", "width": "24", "height": "24" }),
-                                                                        m("path", { "d": "M7.07744993,12.3040451 C7.72444571,13.0716094 8.54044565,13.6920474 9.46808594,14.1079953 L5,23 L4.5,18 L7.07744993,12.3040451 Z M14.5865511,14.2597864 C15.5319561,13.9019016 16.375416,13.3366121 17.0614026,12.6194459 L19.5,18 L19,23 L14.5865511,14.2597864 Z M12,3.55271368e-14 C12.8284271,3.53749572e-14 13.5,0.671572875 13.5,1.5 L13.5,4 L10.5,4 L10.5,1.5 C10.5,0.671572875 11.1715729,3.56793164e-14 12,3.55271368e-14 Z", "fill": "#000000", "opacity": "0.3" }),
-                                                                        m("path", { "d": "M12,10 C13.1045695,10 14,9.1045695 14,8 C14,6.8954305 13.1045695,6 12,6 C10.8954305,6 10,6.8954305 10,8 C10,9.1045695 10.8954305,10 12,10 Z M12,13 C9.23857625,13 7,10.7614237 7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,10.7614237 14.7614237,13 12,13 Z", "fill": "#000000", "fill-rule": "nonzero" })
-                                                                    ]
-                                                                )
-                                                            )
-                                                        )
-                                                    ),
-                                                    // m("div", { "class": "alert-text" },
-                                                    //     [
-                                                    //         "PLEASE DON'T INCLUDE YOUR ",
-                                                    //         m("code",
-                                                    //             "PERSONAL INFORMATION (Phone Number, Email Address) in the instructions section"
-                                                    //         ),
-                                                    //         ". The information is always kept private and we won't share it"
-                                                    //     ]
-                                                    // )
-                                                ]
-                                            )
-                                        ),
+                                        // m("div", { "class": "col-lg-12" },
+                                        //     m("div", { "class": "alert alert-custom alert-default", "role": "alert" },
+                                        //         [
+                                        //             m("div", { "class": "alert-icon" },
+                                        //                 m("span", { "class": "svg-icon svg-icon-primary svg-icon-xl" },
+                                        //                     m("svg", { "xmlns": "http://www.w3.org/2000/svg", "xmlns:xlink": "http://www.w3.org/1999/xlink", "width": "24px", "height": "24px", "viewBox": "0 0 24 24", "version": "1.1" },
+                                        //                         m("g", { "stroke": "none", "stroke-width": "1", "fill": "none", "fill-rule": "evenodd" },
+                                        //                             [
+                                        //                                 m("rect", { "x": "0", "y": "0", "width": "24", "height": "24" }),
+                                        //                                 m("path", { "d": "M7.07744993,12.3040451 C7.72444571,13.0716094 8.54044565,13.6920474 9.46808594,14.1079953 L5,23 L4.5,18 L7.07744993,12.3040451 Z M14.5865511,14.2597864 C15.5319561,13.9019016 16.375416,13.3366121 17.0614026,12.6194459 L19.5,18 L19,23 L14.5865511,14.2597864 Z M12,3.55271368e-14 C12.8284271,3.53749572e-14 13.5,0.671572875 13.5,1.5 L13.5,4 L10.5,4 L10.5,1.5 C10.5,0.671572875 11.1715729,3.56793164e-14 12,3.55271368e-14 Z", "fill": "#000000", "opacity": "0.3" }),
+                                        //                                 m("path", { "d": "M12,10 C13.1045695,10 14,9.1045695 14,8 C14,6.8954305 13.1045695,6 12,6 C10.8954305,6 10,6.8954305 10,8 C10,9.1045695 10.8954305,10 12,10 Z M12,13 C9.23857625,13 7,10.7614237 7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,10.7614237 14.7614237,13 12,13 Z", "fill": "#000000", "fill-rule": "nonzero" })
+                                        //                             ]
+                                        //                         )
+                                        //                     )
+                                        //                 )
+                                        //             ),
+                                        //             m("div", { "class": "alert-text" },
+                                        //                 [
+                                        //                     "PLEASE DON'T INCLUDE YOUR ",
+                                        //                     m("code",
+                                        //                         "PERSONAL INFORMATION (Phone Number, Email Address) in the instructions section"
+                                        //                     ),
+                                        //                     ". The information is always kept private and we won't share it"
+                                        //                 ]
+                                        //             )
+                                        //         ]
+                                        //     )
+                                        // ),
 
                                     ]
                                 )
