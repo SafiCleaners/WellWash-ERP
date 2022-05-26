d4335721f14824f79b6e435ca185a4635a99b588
BransonGitomeh <sirbranson67@gmail.com>
2022-05-26 14:11:37 +0300

    price calculation display and disclaimer

--- client/src/components/calculator.js
+++ client/src/components/calculator.js
@@ -563,35 +563,6 @@ var calculator = () => {
                                         ),
                                         m("br"),
                                         m("br"),
-                                        // m("div", { "class": "col-lg-12" },
-                                        //     m("div", { "class": "alert alert-custom alert-default", "role": "alert" },
-                                        //         [
-                                        //             m("div", { "class": "alert-icon" },
-                                        //                 m("span", { "class": "svg-icon svg-icon-primary svg-icon-xl" },
-                                        //                     m("svg", { "xmlns": "http://www.w3.org/2000/svg", "xmlns:xlink": "http://www.w3.org/1999/xlink", "width": "24px", "height": "24px", "viewBox": "0 0 24 24", "version": "1.1" },
-                                        //                         m("g", { "stroke": "none", "stroke-width": "1", "fill": "none", "fill-rule": "evenodd" },
-                                        //                             [
-                                        //                                 m("rect", { "x": "0", "y": "0", "width": "24", "height": "24" }),
-                                        //                                 m("path", { "d": "M7.07744993,12.3040451 C7.72444571,13.0716094 8.54044565,13.6920474 9.46808594,14.1079953 L5,23 L4.5,18 L7.07744993,12.3040451 Z M14.5865511,14.2597864 C15.5319561,13.9019016 16.375416,13.3366121 17.0614026,12.6194459 L19.5,18 L19,23 L14.5865511,14.2597864 Z M12,3.55271368e-14 C12.8284271,3.53749572e-14 13.5,0.671572875 13.5,1.5 L13.5,4 L10.5,4 L10.5,1.5 C10.5,0.671572875 11.1715729,3.56793164e-14 12,3.55271368e-14 Z", "fill": "#000000", "opacity": "0.3" }),
-                                        //                                 m("path", { "d": "M12,10 C13.1045695,10 14,9.1045695 14,8 C14,6.8954305 13.1045695,6 12,6 C10.8954305,6 10,6.8954305 10,8 C10,9.1045695 10.8954305,10 12,10 Z M12,13 C9.23857625,13 7,10.7614237 7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,10.7614237 14.7614237,13 12,13 Z", "fill": "#000000", "fill-rule": "nonzero" })
-                                        //                             ]
-                                        //                         )
-                                        //                     )
-                                        //                 )
-                                        //             ),
-                                        //             m("div", { "class": "alert-text" },
-                                        //                 [
-                                        //                     "PLEASE DON'T INCLUDE YOUR ",
-                                        //                     m("code",
-                                        //                         "PERSONAL INFORMATION (Phone Number, Email Address) in the instructions section"
-                                        //                     ),
-                                        //                     ". The information is always kept private and we won't share it"
-                                        //                 ]
-                                        //             )
-                                        //         ]
-                                        //     )
-                                        // ),
-
                                     ]
                                 ),
 
@@ -630,9 +601,9 @@ var calculator = () => {
                                             name: 'Blankets',
                                             value: 0,
                                             charge: 350,
-                                            value: curtains,
+                                            value: blankets,
                                             onChange(value) {
-                                                vnode.state.curtains = value
+                                                vnode.state.blankets = value
                                             }
                                         }),
                                         m(input, {
@@ -652,7 +623,14 @@ var calculator = () => {
                                             onChange(value) {
                                                 vnode.state.generalKgs = value
                                             }
-                                        })
+                                        }),
+
+                                        m("h3", { "class": "display-4" },
+                                            `This would cost around KSH ${(curtains * 200) + (blankets * 350) + (duvets * 700) + (generalKgs * 99)}`
+                                        ),
+                                        m("p", { "class": "font-size-lg" },
+                                            `During Pickup a weigh will be done on premise to collect the exact details for a better estimate`
+                                        )
                                     ]),
 
 
