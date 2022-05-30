2377d34bbe318fdd127eb16e28a2f8a692f49e0a
BransonGitomeh <sirbranson67@gmail.com>
2022-05-30 12:51:05 +0300

    chore: try read from the constants file. and use hostname in location

--- client/src/components/google_login.js
+++ client/src/components/google_login.js
@@ -5,97 +5,98 @@ import m from "mithril";
 import createAuth0Client from "@auth0/auth0-spa-js";
 
 const google_login = {
-  async oncreate() {
-    //with async/await
+    async oncreate() {
+        //with async/await
 
-    const auth0 = await createAuth0Client({
-      domain: "localhost",
-      client_id: "Rh1IviNqvnuubqFKoC9FVESpgUFb6Ka8",
-      redirect_uri: "<MY_CALLBACK_URL>",
-    });
+        const auth0 = await createAuth0Client({
+            domain: window.location.hostname,
+            //   client_id: "Rh1IviNqvnuubqFKoC9FVESpgUFb6Ka8",
+            client_id,
+            // redirect_uri: "<MY_CALLBACK_URL>",
+        });
 
-    const isAuthenticated = await auth0.isAuthenticated();
+        const isAuthenticated = await auth0.isAuthenticated();
 
-    if (isAuthenticated) {
-      console.log("> User is authenticated");
+        if (isAuthenticated) {
+            console.log("> User is authenticated");
 
-      return;
-    }
-  },
-  view() {
-    return m(
-      "button",
-      {
-        class: "btn btn-danger btn-lg",
-        onclick() {
-          // try Oauth
-          //   return login();
-          function init() {
-            window.gapi.load("auth2", function () {
-              console.log("this is gapi>>>>>>>>>>>>>>>>>>", window.gapi.auth2);
-              //all auth stuff needing gapi.auth2
+            return;
+        }
+    },
+    view() {
+        return m(
+            "button",
+            {
+                class: "btn btn-danger btn-lg",
+                onclick() {
+                    // try Oauth
+                    //   return login();
+                    function init() {
+                        window.gapi.load("auth2", function () {
+                            console.log("this is gapi>>>>>>>>>>>>>>>>>>", window.gapi.auth2);
+                            //all auth stuff needing gapi.auth2
 
-              //initialise gapi
-              window.gapi.auth2.init({
-                client_id: client_id,
-              });
-              const GoogleAuth = window.gapi.auth2.getAuthInstance();
+                            //initialise gapi
+                            window.gapi.auth2.init({
+                                client_id: client_id,
+                            });
+                            const GoogleAuth = window.gapi.auth2.getAuthInstance();
 
-              const options = {
-                prompt: "consent",
-              };
+                            const options = {
+                                prompt: "consent",
+                            };
 
-              GoogleAuth.signIn(options).then(
-                (googleUser) => {
-                  let profile = googleUser.getBasicProfile();
-                  localStorage.setItem("authToken", profile.getId());
-                  localStorage.setItem("name", profile.getName());
-                  localStorage.setItem("imageUrl", profile.getImageUrl());
-                  localStorage.setItem("email", profile.getEmail());
+                            GoogleAuth.signIn(options).then(
+                                (googleUser) => {
+                                    let profile = googleUser.getBasicProfile();
+                                    localStorage.setItem("authToken", profile.getId());
+                                    localStorage.setItem("name", profile.getName());
+                                    localStorage.setItem("imageUrl", profile.getImageUrl());
+                                    localStorage.setItem("email", profile.getEmail());
 
-                  const userData = {
-                    id: profile.getId(),
-                    name: profile.getName(),
-                    imageUrl: profile.getImageUrl(),
-                    email: profile.getEmail(),
-                  };
+                                    const userData = {
+                                        id: profile.getId(),
+                                        name: profile.getName(),
+                                        imageUrl: profile.getImageUrl(),
+                                        email: profile.getEmail(),
+                                    };
 
-                  // refresh after successfull login
-                  const options = {
-                    method: "POST",
-                    url: url + "/users",
-                    headers: {
-                      "Content-Type": "application/json",
-                      // 'authorization': localStorage.getItem('token')
-                    },
-                    data: userData,
-                  };
+                                    // refresh after successfull login
+                                    const options = {
+                                        method: "POST",
+                                        url: url + "/users",
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                            // 'authorization': localStorage.getItem('token')
+                                        },
+                                        data: userData,
+                                    };
 
-                  axios
-                    .request(options)
-                    .then(function (response) {
-                      // alert(JSON.stringify(response.data));
-                      localStorage.setItem("token", response.data.token);
-                      localStorage.setItem("role", response.data.user.role);
-                      location.reload();
-                    })
-                    .catch(function (error) {
-                      console.error(error);
-                    });
+                                    axios
+                                        .request(options)
+                                        .then(function (response) {
+                                            // alert(JSON.stringify(response.data));
+                                            localStorage.setItem("token", response.data.token);
+                                            localStorage.setItem("role", response.data.user.role);
+                                            location.reload();
+                                        })
+                                        .catch(function (error) {
+                                            console.error(error);
+                                        });
+                                },
+                                (err) => {
+                                    console.error(err);
+                                }
+                            );
+                        });
+                    }
+                    init();
                 },
-                (err) => {
-                  console.error(err);
-                }
-              );
-            });
-          }
-          init();
-        },
-      },
-      m("i", { class: "icon-xl fab fa-google" }),
-      "Sign In"
-    );
-  },
+            },
+            m("i", { class: "icon-xl fab fa-google" }),
+            "Sign In"
+        );
+    },
 };
 
 export default google_login;
