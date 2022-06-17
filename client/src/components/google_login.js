dd01c3cae49291be45d3ff9132a145efa36501d7
Oneted11 <muriithited@gmail.com>
2022-06-17 09:55:42 +0300

    feat:added gsi client to index-dev

--- client/src/components/google_login.js
+++ client/src/components/google_login.js
@@ -5,108 +5,112 @@ import m from "mithril";
 import createAuth0Client from "@auth0/auth0-spa-js";
 
 const google_login = {
-    async oncreate() {
-        //with async/await
+  async oncreate() {
+    //with async/await
 
-        function init() {
-            window.gapi.load("auth2", async () => {
-                console.log("this is gapi>>>>>>>>>>>>>>>>>>", window.gapi.auth2);
-                //all auth stuff needing gapi.auth2
+    function init() {
+      window.gapi.load("auth2", async () => {
+        console.log("this is gapi>>>>>>>>>>>>>>>>>>", window.gapi.auth2);
+        //all auth stuff needing gapi.auth2
 
-                //initialise gapi
-                window.gapi.auth2.init({
-                    client_id,
-                });
+        //initialise gapi
+        window.gapi.auth2.init({
+          client_id,
+        });
 
-                const isAuthenticated = await window.gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse();
+        const isAuthenticated = await window.gapi.auth2
+          .getAuthInstance()
+          .currentUser.get()
+          .getAuthResponse();
 
-                if (isAuthenticated) {
-                    console.log("> User is authenticated", { isAuthenticated });
+        if (isAuthenticated) {
+          console.log("> User is authenticated", { isAuthenticated });
+          console.log("user>>>>>>>>>>>>>");
 
-                    return;
-                }
-
-            })
+          return;
         }
+      });
+    }
 
-        init()
+    init();
+  },
+  view() {
+    return m(
+      "button",
+      {
+        class: "btn btn-danger btn-lg",
+        onclick() {
+          // try Oauth
+          //   return login();
+          function init() {
+            window.gapi.load("auth2", function () {
+              //all auth stuff needing gapi.auth2
 
+              //initialise gapi
+              window.gapi.auth2.init({
+                client_id: client_id,
+              });
+              console.log(
+                "this is gapi>>>>>>>>>>>>>>>>>>",
+                window.gapi.auth2.getBasicProfile()
+              );
+              const GoogleAuth = window.gapi.auth2.getAuthInstance();
 
-    },
-    view() {
-        return m(
-            "button",
-            {
-                class: "btn btn-danger btn-lg",
-                onclick() {
-                    // try Oauth
-                    //   return login();
-                    function init() {
-                        window.gapi.load("auth2", function () {
-                            console.log("this is gapi>>>>>>>>>>>>>>>>>>", window.gapi.auth2);
-                            //all auth stuff needing gapi.auth2
+              const options = {
+                // prompt: "consent",
+              };
 
-                            //initialise gapi
-                            window.gapi.auth2.init({
-                                client_id,
-                            });
-                            const GoogleAuth = window.gapi.auth2.getAuthInstance();
+              GoogleAuth.signIn(options).then(
+                (googleUser) => {
+                  let profile = googleUser.getBasicProfile();
+                  localStorage.setItem("authToken", profile.getId());
+                  localStorage.setItem("name", profile.getName());
+                  localStorage.setItem("imageUrl", profile.getImageUrl());
+                  localStorage.setItem("email", profile.getEmail());
 
-                            const options = {
-                                // prompt: "consent",
-                            };
+                  const userData = {
+                    id: profile.getId(),
+                    name: profile.getName(),
+                    imageUrl: profile.getImageUrl(),
+                    email: profile.getEmail(),
+                  };
 
-                            GoogleAuth.signIn(options).then(
-                                (googleUser) => {
-                                    let profile = googleUser.getBasicProfile();
-                                    localStorage.setItem("authToken", profile.getId());
-                                    localStorage.setItem("name", profile.getName());
-                                    localStorage.setItem("imageUrl", profile.getImageUrl());
-                                    localStorage.setItem("email", profile.getEmail());
+                  // refresh after successfull login
+                  const options = {
+                    method: "POST",
+                    url: url + "/users",
+                    headers: {
+                      "Content-Type": "application/json",
+                      // 'authorization': localStorage.getItem('token')
+                    },
+                    data: userData,
+                  };
 
-                                    const userData = {
-                                        id: profile.getId(),
-                                        name: profile.getName(),
-                                        imageUrl: profile.getImageUrl(),
-                                        email: profile.getEmail(),
-                                    };
-
-                                    // refresh after successfull login
-                                    const options = {
-                                        method: "POST",
-                                        url: url + "/users",
-                                        headers: {
-                                            "Content-Type": "application/json",
-                                            // 'authorization': localStorage.getItem('token')
-                                        },
-                                        data: userData,
-                                    };
-
-                                    axios
-                                        .request(options)
-                                        .then(function (response) {
-                                            // alert(JSON.stringify(response.data));
-                                            localStorage.setItem("token", response.data.token);
-                                            localStorage.setItem("role", response.data.user.role);
-                                            location.reload();
-                                        })
-                                        .catch(function (error) {
-                                            console.error(error);
-                                        });
-                                },
-                                (err) => {
-                                    console.error(err);
-                                }
-                            );
-                        });
-                    }
-                    init();
+                  axios
+                    .request(options)
+                    .then(function (response) {
+                      // alert(JSON.stringify(response.data));
+                      localStorage.setItem("token", response.data.token);
+                      localStorage.setItem("role", response.data.user.role);
+                      location.reload();
+                    })
+                    .catch(function (error) {
+                      console.error(error);
+                    });
                 },
-            },
-            m("i", { class: "icon-xl fab fa-google" }),
-            "Sign In"
-        );
-    },
+                (err) => {
+                  console.error(err);
+                }
+              );
+            });
+          }
+          init();
+        },
+      },
+      m("i", { class: "icon-xl fab fa-google" }),
+      "Sign In"
+    );
+  },
 };
 
 export default google_login;
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,14 +1,9 @@
-var client_id = "414476019901-fbo1ktmntoc7slhhr9ialqbhcflr928j.apps.googleusercontent.com";
-let url = 'https://smartpaperwriters.herokuapp.com'
+var client_id =
+  "345449971495-4lncidafh4k98fqnr16a5bp76nmgveha.apps.googleusercontent.com";
+let url = "https://smartpaperwriters.herokuapp.com";
 
-if ([
-    "localhopst",
-    "127.0.0.1"
-].includes(window.location.hostname)) {
-    url = 'http://localhost:8002'
+if (["localhost", "127.0.0.1"].includes(window.location.hostname)) {
+  url = "http://localhost:8002";
 }
 
-export {
-    client_id,
-    url
-}
+export { client_id, url };
