d1b6706385529ea8a83c8d2d871eea9500d976eb
BransonGitomeh <sirbranson67@gmail.com>
2022-12-20 11:42:07 +0300

    fix: restore login with google button. move whatsapp buttonto right.

--- client/src/components/google_login.js
+++ client/src/components/google_login.js
@@ -85,73 +85,73 @@ const google_login = {
     };
   },
   view() {
-    return []
-    // return m(
-    //   "button",
-    //   {
-    //     class: "btn btn-danger btn-lg",
-    //     onclick() {
-    //       // try Oauth
-    //       //   return login();
-    //       function init() {
-    //         window.gapi.load("auth2", function () {
-    //           //all auth stuff needing gapi.auth2
+    // return []
+    return m(
+      "button",
+      {
+        class: "btn btn-danger btn-lg",
+        onclick() {
+          // try Oauth
+          //   return login();
+          function init() {
+            window.gapi.load("auth2", function () {
+              //all auth stuff needing gapi.auth2
 
-    //           //initialise gapi
-    //           window.gapi.auth2.init({
-    //             client_id: client_id,
-    //           });
+              //initialise gapi
+              window.gapi.auth2.init({
+                client_id: client_id,
+              });
 
-    //           Promise.resolve(window.gapi.auth2.getAuthInstance().signIn()).then(
-    //             (googleUser) => {
-    //               let profile = googleUser.getBasicProfile();
-    //               localStorage.setItem("authToken", profile.getId());
-    //               localStorage.setItem("name", profile.getName());
-    //               localStorage.setItem("imageUrl", profile.getImageUrl());
-    //               localStorage.setItem("email", profile.getEmail());
+              Promise.resolve(window.gapi.auth2.getAuthInstance().signIn()).then(
+                (googleUser) => {
+                  let profile = googleUser.getBasicProfile();
+                  localStorage.setItem("authToken", profile.getId());
+                  localStorage.setItem("name", profile.getName());
+                  localStorage.setItem("imageUrl", profile.getImageUrl());
+                  localStorage.setItem("email", profile.getEmail());
 
-    //               const userData = {
-    //                 id: profile.getId(),
-    //                 name: profile.getName(),
-    //                 imageUrl: profile.getImageUrl(),
-    //                 email: profile.getEmail(),
-    //               };
+                  const userData = {
+                    id: profile.getId(),
+                    name: profile.getName(),
+                    imageUrl: profile.getImageUrl(),
+                    email: profile.getEmail(),
+                  };
 
-    //               // refresh after successfull login
-    // const options = {
-    //   method: "POST",
-    //   url: url + "/users",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     // 'authorization': localStorage.getItem('token')
-    //   },
-    //   data: userData,
-    // };
+                  // refresh after successfull login
+                  const options = {
+                    method: "POST",
+                    url: url + "/users",
+                    headers: {
+                      "Content-Type": "application/json",
+                      // 'authorization': localStorage.getItem('token')
+                    },
+                    data: userData,
+                  };
 
-    // axios
-    //   .request(options)
-    //   .then(function (response) {
-    //     // alert(JSON.stringify(response.data));
-    //     localStorage.setItem("token", response.data.token);
-    //     localStorage.setItem("role", response.data.user.role);
-    //     location.reload();
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
-    //             },
-    //             (err) => {
-    //               console.error(err);
-    //             }
-    //           );
-    //         });
-    //       }
-    //       init();
-    //     },
-    //   },
-    //   m("i", { class: "icon-xl fab fa-google" }),
-    //   "Sign In"
-    // );
+                  axios
+                    .request(options)
+                    .then(function (response) {
+                      // alert(JSON.stringify(response.data));
+                      localStorage.setItem("token", response.data.token);
+                      localStorage.setItem("role", response.data.user.role);
+                      location.reload();
+                    })
+                    .catch(function (error) {
+                      console.error(error);
+                    });
+                },
+                (err) => {
+                  console.error(err);
+                }
+              );
+            });
+          }
+          init();
+        },
+      },
+      m("i", { class: "icon-xl fab fa-google" }),
+      "Sign In"
+    );
   },
 };
 
