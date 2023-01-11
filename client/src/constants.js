aae8ba60c2422c39aa79a4fca1fadf3403b5646f
BransonGitomeh <sirbranson67@gmail.com>
2023-01-11 21:14:43 +0300

    chore: remove netlify functions and add working heroku url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 var client_id =
   "345449971495-4lncidafh4k98fqnr16a5bp76nmgveha.apps.googleusercontent.com";
-let url = "https://smartpaperwriters.herokuapp.com";
+let url = "https://wellwash.herokuapp.com/";
 
 if (["localhost", "127.0.0.1"].includes(window.location.hostname)) {
   url = "http://localhost:8002";
