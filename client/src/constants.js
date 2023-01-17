8e9607e5fd0bf90d4af77e08c6a5dd7abce839eb
BransonGitomeh <sirbranson67@gmail.com>
2023-01-17 14:36:21 +0300

    fix: url typo

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 var client_id =
   "345449971495-4lncidafh4k98fqnr16a5bp76nmgveha.apps.googleusercontent.com";
   // "345449971495-h5hr0g4bs8tiq2u0kk4dl1icofavq4bg.apps.googleusercontent.com";
-  let url = "https:/wellwash.herokuapp.com";
+  let url = "https://wellwash.herokuapp.com";
 
 if (["localhost", "127.0.0.1"].includes(window.location.hostname)) {
   url = "http://localhost:8002";
