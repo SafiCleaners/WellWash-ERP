1dcdb7dc92401bf49f36fb2a55fa304aa63f9251
Oneted11 <muriithited@gmail.com>
2023-01-17 12:18:01 +0300

    fix:google login on local

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 var client_id =
-  // "345449971495-4lncidafh4k98fqnr16a5bp76nmgveha.apps.googleusercontent.com";
-  "345449971495-h5hr0g4bs8tiq2u0kk4dl1icofavq4bg.apps.googleusercontent.com";
+  "345449971495-4lncidafh4k98fqnr16a5bp76nmgveha.apps.googleusercontent.com";
+  // "345449971495-h5hr0g4bs8tiq2u0kk4dl1icofavq4bg.apps.googleusercontent.com";
 let url = "https://wellwash.online";
 
 if (["localhost", "127.0.0.1"].includes(window.location.hostname)) {
