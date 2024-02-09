49576ffeb575deb7493dfb0526da44b57da509ff
BransonGitomeh <sirbranson67@gmail.com>
2024-02-09 11:26:07 +0300

    fix: sidebar sugar on small devices

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -45,10 +45,41 @@ const header = {
         return m("div", { "class": "header header-fixed", "id": "kt_header" },
             m("div", { "class": "container" },
                 [
-                    m("div", { "class": "header-menu-wrapper header-menu-wrapper-left", "id": "kt_header_menu_wrapper" },
+                    m("div", {
+                        "class": "header-menu-wrapper header-menu-wrapper-left", "id": "kt_header_menu_wrapper", style: {
+                            "margin": "20px",
+                            "border-radius": "20px"
+                        }
+                    },
                         m("div", { "class": "header-menu header-menu-left header-menu-mobile header-menu-layout-default", "id": "kt_header_menu" },
                             m("ul", { "class": "menu-nav" },
                                 [
+                                    !localStorage.getItem('authToken') ? [] : [ m("li.d-block.d-sm-none", {
+                                        "class": "menu-item", "aria-haspopup": "true"
+                                    },
+                                        m(m.route.Link, { "class": "menu-link", "href": "/" },
+                                            
+                                            [
+                                                m("span", { "class": "svg-icon svg-icon-xl menu-icon" }, [
+                                                m("img", {
+                                                    src: localStorage.getItem('imageUrl'),
+                                                    style: {
+                                                        "max-width": "100%",
+                                                        height: "auto"
+                                                    }
+                                                })
+                                            ]),
+                                                m("span", { "class": "menu-text", style:{
+                                                    "display": "flex",
+                                                    "flex-direction": "column",
+
+                                                } },  [
+                                                    localStorage.getItem('name'),
+                                                    m("small.text-muted.ml-2",localStorage.getItem('email'))    
+                                            ])
+                                            ]
+                                        )
+                                    )],
                                     m("li", {
                                         "class": "menu-item" + (
                                             ["/", ""].includes(window.location.pathname) ? " menu-item-active" : ""), "aria-haspopup": "true"
@@ -114,84 +145,84 @@ const header = {
                                         )
                                     ] : [
                                         m("li", { "class": "menu-item" + (window.location.pathname.includes("dash") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                        m("a", { "class": "menu-link", "href": "/dash" },
-                                            [
-                                                m("span", { "class": "menu-text" },
-                                                    "Dashboard"
-                                                ),
-                                                m("span", { "class": "menu-desc" })
-                                            ]
-                                        )
-                                            ), m("li", { "class": "menu-item" + (window.location.pathname.includes("j") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                                m("a", { "class": "menu-link", "href": "/j" },
-                                                    [
-                                                        m("span", { "class": "menu-text" },
-                                                            "Queue"
-                                                        ),
-                                                        m("span", { "class": "menu-desc" })
-                                                    ]
-                                                )
-                                            ),
-                                            // m("li", { "class": "menu-item" + (window.location.pathname.includes("expense") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                            //     m("a", { "class": "menu-link", "href": "/e" },
-                                            //         [
-                                            //             m("span", { "class": "menu-text" },
-                                            //                 "Expenses"
-                                            //             ),
-                                            //             m("span", { "class": "menu-desc" })
-                                            //         ]
-                                            //     )
-                                            // ),
-                                    m("li", { "class": "menu-item" + (window.location.pathname.includes("users") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                        m(m.route.Link, { "class": "menu-link", "href": "/users" },
-                                            [
-                                                m("span", { "class": "menu-text" },
-                                                    "Users"
-                                                ),
-                                                m("span", { "class": "menu-desc" })
-                                            ]
-                                        )
-                                    ),
-                                    m("li", { "class": "menu-item" + (window.location.pathname.includes("brands") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                        m(m.route.Link, { "class": "menu-link", "href": "/brands" },
-                                            [
-                                                m("span", { "class": "menu-text" },
-                                                    "Brands"
-                                                ),
-                                                m("span", { "class": "menu-desc" })
-                                            ]
-                                        )
-                                    ),
-                                    m("li", { "class": "menu-item" + (window.location.pathname.includes("stores") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                        m(m.route.Link, { "class": "menu-link", "href": "/stores" },
-                                            [
-                                                m("span", { "class": "menu-text" },
-                                                    "Stores"
-                                                ),
-                                                m("span", { "class": "menu-desc" })
-                                            ]
-                                        )
-                                    ),
-                                    m("li", { "class": "menu-item" + (window.location.pathname.includes("pricing") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                        m(m.route.Link, { "class": "menu-link", "href": "/pricing" },
-                                            [
-                                                m("span", { "class": "menu-text" },
-                                                    "Pricing"
-                                                ),
-                                                m("span", { "class": "menu-desc" })
-                                            ]
-                                        )
-                                    ),
-                                    m("li", { "class": "menu-item" + (window.location.pathname.includes("clients") ? " menu-item-active" : ""), "aria-haspopup": "true" },
-                                        m(m.route.Link, { "class": "menu-link", "href": "/clients" },
-                                            [
-                                                m("span", { "class": "menu-text" },
-                                                    "Clients"
-                                                ),
-                                                m("span", { "class": "menu-desc" })
-                                            ]
-                                        )
-                                    ),
+                                            m("a", { "class": "menu-link", "href": "/dash" },
+                                                [
+                                                    m("span", { "class": "menu-text" },
+                                                        "Dashboard"
+                                                    ),
+                                                    m("span", { "class": "menu-desc" })
+                                                ]
+                                            )
+                                        ), m("li", { "class": "menu-item" + (window.location.pathname.includes("j") ? " menu-item-active" : ""), "aria-haspopup": "true" },
+                                            m("a", { "class": "menu-link", "href": "/j" },
+                                                [
+                                                    m("span", { "class": "menu-text" },
+                                                        "Queue"
+                                                    ),
+                                                    m("span", { "class": "menu-desc" })
+                                                ]
+                                            )
+                                        ),
+                                        // m("li", { "class": "menu-item" + (window.location.pathname.includes("expense") ? " menu-item-active" : ""), "aria-haspopup": "true" },
+                                        //     m("a", { "class": "menu-link", "href": "/e" },
+                                        //         [
+                                        //             m("span", { "class": "menu-text" },
+                                        //                 "Expenses"
+                                        //             ),
+                                        //             m("span", { "class": "menu-desc" })
+                                        //         ]
+                                        //     )
+                                        // ),
+                                        m("li", { "class": "menu-item" + (window.location.pathname.includes("users") ? " menu-item-active" : ""), "aria-haspopup": "true" },
+                                            m(m.route.Link, { "class": "menu-link", "href": "/users" },
+                                                [
+                                                    m("span", { "class": "menu-text" },
+                                                        "Users"
+                                                    ),
+                                                    m("span", { "class": "menu-desc" })
+                                                ]
+                                            )
+                                        ),
+                                        m("li", { "class": "menu-item" + (window.location.pathname.includes("brands") ? " menu-item-active" : ""), "aria-haspopup": "true" },
+                                            m(m.route.Link, { "class": "menu-link", "href": "/brands" },
+                                                [
+                                                    m("span", { "class": "menu-text" },
+                                                        "Brands"
+                                                    ),
+                                                    m("span", { "class": "menu-desc" })
+                                                ]
+                                            )
+                                        ),
+                                        m("li", { "class": "menu-item" + (window.location.pathname.includes("stores") ? " menu-item-active" : ""), "aria-haspopup": "true" },
+                                            m(m.route.Link, { "class": "menu-link", "href": "/stores" },
+                                                [
+                                                    m("span", { "class": "menu-text" },
+                                                        "Stores"
+                                                    ),
+                                                    m("span", { "class": "menu-desc" })
+                                                ]
+                                            )
+                                        ),
+                                        m("li", { "class": "menu-item" + (window.location.pathname.includes("pricing") ? " menu-item-active" : ""), "aria-haspopup": "true" },
+                                            m(m.route.Link, { "class": "menu-link", "href": "/pricing" },
+                                                [
+                                                    m("span", { "class": "menu-text" },
+                                                        "Pricing"
+                                                    ),
+                                                    m("span", { "class": "menu-desc" })
+                                                ]
+                                            )
+                                        ),
+                                        m("li", { "class": "menu-item" + (window.location.pathname.includes("clients") ? " menu-item-active" : ""), "aria-haspopup": "true" },
+                                            m(m.route.Link, { "class": "menu-link", "href": "/clients" },
+                                                [
+                                                    m("span", { "class": "menu-text" },
+                                                        "Clients"
+                                                    ),
+                                                    m("span", { "class": "menu-desc" })
+                                                ]
+                                            )
+                                        ),
 
                                         // m("li", { "class": "menu-item" + (window.location.pathname.includes("orders") ? " menu-item-active" : ""), "aria-haspopup": "true" },
                                         //     m(m.route.Link, { "class": "menu-link", "href": "/orders" },
@@ -225,7 +256,7 @@ const header = {
                             m("div", { "class": "topbar-item mr-1", "data-toggle": "dropdown", "data-offset": "10px,0px", "aria-expanded": "false" }, [
                                 m("button", { "class": "btn btn-md btn-secondary dropdown-toggle", "type": "button", "id": "dropdownMenuButton", "data-toggle": "dropdown", "aria-haspopup": "true", "aria-expanded": "false" },
                                     // " All Stores "
-                                    !localStorage.getItem('storeId')  ? " All Stores " :  vnode.state.stores?.filter(store => store._id == localStorage.getItem('storeId'))[0]?.title
+                                    !localStorage.getItem('storeId') ? " All Stores " : vnode.state.stores?.filter(store => store._id == localStorage.getItem('storeId'))[0]?.title
                                 ),
                                 m("div", { "class": "w-auto d-flex align-items-center btn-md px-2", "id": "kt_quick_user_toggle" },
                                     m("div", { "class": "dropdown-menu", "aria-labelledby": "dropdownMenuButton" },
@@ -256,7 +287,7 @@ const header = {
 
                                                         m.redraw()
                                                     }
-                                                },  store.title);
+                                                }, store.title);
                                             })
                                         ]
                                     )),
@@ -264,7 +295,7 @@ const header = {
                             m("div", { "class": "topbar-item mr-1 w-auto d-flex align-items-center btn-md px-2", "id": "kt_quick_user_toggle" },
                                 m(DateRangePicker, {
                                     // "class": "form-control form-control-solid",
-                                    class:"btn btn-md btn-secondary dropdown-toggle",
+                                    class: "btn btn-md btn-secondary dropdown-toggle",
                                     "placeholder": "Select Business Day",
                                     // "id": "kt_daterangepicker_new",
                                     onChange: onDatePickerChange
@@ -294,7 +325,7 @@ const header = {
                                         window.location.reload()
                                     }
                                 },
-                                    m("i", { class: "fa fa-power-off", "aria-hidden":"true"})
+                                    m("i", { class: "fa fa-power-off", "aria-hidden": "true" })
                                 )
                             )
                         ] : m("div", { style: { margin: "auto" } }, [
