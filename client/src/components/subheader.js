bd1a0eef4f207bebeed9e18e03612e8c8869dfb7
BransonGitomeh <sirbranson67@gmail.com>
2024-04-15 17:27:17 +0300

    fix: add ability to render and ingest brand logos

--- client/src/components/subheader.js
+++ client/src/components/subheader.js
@@ -1,15 +1,43 @@
 import m from "mithril"
+import axios from "axios";
 import google_login from "./google_login"
+import {
+    url
+} from "../constants"
 
 const subheader = () => {
     return {
-        view() {
+        oninit(vnode){
+            vnode.state.brands = []
+        },
+        oncreate(vnode) {
+            const options = {
+                method: 'GET', url: url + "/brands",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': localStorage.getItem('token')
+                },
+            };
+    
+            axios.request(options).then(function (response) {
+                vnode.state.brands = response.data
+                vnode.state.loading = false
+                m.redraw()
+            }).catch(function (error) {
+                vnode.state.loading = false
+                m.redraw()
+                console.error(error);
+            });
+        },
+        view(vnode) {
+            var brandLogo = vnode.state.brands.find(b=>b._id==localStorage.getItem('brand'))?.logo
+
             return !["/", "q-new"].includes(m.route.get()) ? [] : m("div", { "class": "subheader bg-white h-100px", "id": "kt_subheader" },
                 m("div", { "class": "container flex-wrap flex-sm-nowrap" },
                     [
                         m("div", { "class": "d-none d-lg-flex align-items-center flex-wrap w-300px" },
                             m(m.route.Link, { "href": "/" },
-                                m("img", { "class": "max-h-100px", "alt": "Logo", "src": "/assets/media/exported-wellwash-new.png" })
+                                m("img", { "class": "max-h-100px", "alt": "Logo", "src": brandLogo ? brandLogo : "/assets/media/exported-wellwash-new.png" })
                             )
                         ),
                         m("div", { "class": "subheader-nav nav flex-grow-1" },
